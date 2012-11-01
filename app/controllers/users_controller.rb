012227e8c29779a63f3ef91002622fb443da7f84
alessandrodimich <alessandrodimich@hotmail.com>
2012-11-01 21:10:14 +0100

    almost finished CHAPTER7-SIGNUP except for the password_digest validation message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,18 @@ class UsersController < ApplicationController
 	end
 
 	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			flash[:success] = "Welcome to my new App!!"
+			redirect_to user_path(@user)
+		else
+			render 'new'
+	end
+
 	end
 	
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,17 +6,66 @@ describe "User pages" do
 
   describe "signup page" do
     before { visit signup_path }
+
     it { should have_selector('h1',    text: 'Sign up') }
-    it {should have_selector('title', text: 'Sign up')}
+    it { should have_selector('title', text: 'Sign up') }
   end
+
 
   describe "profile page" do
-  	let(:factory_user) {FactoryGirl.create(:user)}  # User comes from factories.rb where it references the user model
-  	before { visit user_path(factory_user) }
+    let (:foobar) { FactoryGirl.create(:user)}  #let is used to create a local variable, whereas the symbol  :user passed to 
+  											#factory girl has to be the same as in factories.rb.
+    before { visit user_path(foobar) }
 
-  	it {should have_selector('h1', text: factory_user.full_name)}
-  	it {should have_selector('title', text: factory_user.full_name)}
+    it { should have_selector('h1',    text: foobar.full_name) }
+    it { should have_selector('title', text: foobar.full_name) }
   end
 
+  describe "signup" do
+
+    before {visit signup_path}
+
+    let(:submit) {"Create my account"}
+    
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe "After submission" do
+        before {click_button submit}
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+        it { should_not have_content('Password digest')}
+      end
+    end
+
+    describe "with valid information" do
+
+      before do
+        fill_in "user_first_name",   with: "Example"
+        fill_in "user_last_name",    with: "User"
+        fill_in "user_nickname",     with: "foobarf"
+        fill_in "user_email",        with: "user@example.com"
+        fill_in "user_password",     with: "foobar2"
+        fill_in "user_password_confirmation", with: "foobar2"
+      end
+
+      it "should create a new user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    
+    describe "After saving a user" do
+    
+      before { click_button submit }
+
+      let(:user) {User.find_by_email("user@example.com")}
+
+      it { should have_selector('title', text: user.full_name) }
+
+    end
+  end
+
+end
 
 end
